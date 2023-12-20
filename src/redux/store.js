97c3313df8d69447f623245ac795a8ee9a814d64
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-12-20 22:45:23 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import { contactsReducer } from './contactsSlice';
+
 
 export const store = configureStore({
   reducer: {
